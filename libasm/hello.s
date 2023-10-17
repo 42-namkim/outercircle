c0b501923f0b8b5a613fdf4ad3170223668f8fe9
namkim <namkim@student.42seoul.kr>
2023-10-17 22:36:03 +0900

    error : invalid system call

--- libasm/hello.s
+++ libasm/hello.s
@@ -1,14 +1,21 @@
-          global    _main
+        global    _main
 
-          section   .text
-_main:    mov       rax, 0x02000004         ; system call for write
-          mov       rdi, 1                  ; file handle 1 is stdout
-          mov       rsi, message            ; address of string to output
-          mov       rdx, 5                 ; number of bytes (if i write 5, it only writes 5)
-          syscall                           ; invoke operating system to do the write
-          mov       rax, 0x02000001         ; system call for exit
-          xor       rdi, rdi                ; exit code 0
-          syscall                           ; invoke operating system to exit
+        section         .text
+_main:
+        xor     rax, 0x02000003 ; sys_read
+        xor     rdi, rdi ; fd == stdin
+        mov     rsi, buf ; buf as buf addr
+        mov     rdx, 80  ; set buf size
+        syscall
 
-          section   .data
-message:  db        "Hello, World", 10      ; note the newline at the end (what is 10 at the end?)
+        mov     rax, 0x02000004  ; sys_write
+        mov     rdi, 1  ; fd == stdout
+        mov     rdx, 80 ; buffer size ... why should i set this again?
+        syscall
+
+        mov     rax, 0x02000001 ; sys_exit
+        xor     rdi, rdi ; exitcode = 0
+        syscall
+
+        section         .bss
+buf:   resb      80
