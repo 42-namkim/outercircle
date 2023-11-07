0bb70d329e58a3310257e7b9a0c332bd7bff0408
namkim <namkim@student.42seoul.kr>
2023-11-07 08:19:05 +0000

    python piscine - 0.starting : finish ex02

---
+++ python_piscine/0_starting/ex02/find_ft_type.py
@@ -0,0 +1,19 @@
+def all_thing_is_obj(object: any) -> int:
+    types = {list: "List"
+            , tuple: "Tuple"
+            , set: "Set"
+            , dict: "Dict"
+            , str: " is in the kitchen"
+            }
+    for t in types:
+        if isinstance(object, t):
+                if t == str:
+                    print(object + types[t], end='')
+                else:
+                    print(types[t], end='')
+                print(" : ", end='')
+                print(type(object))
+                break
+    if type(object) not in types:
+        print("Type not found")
+    return 42
