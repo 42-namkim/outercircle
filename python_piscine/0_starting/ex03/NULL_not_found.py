06e5fcfed6609434c7a4103aaac047271ddd769f
namkim <namkim@student.42seoul.kr>
2023-11-07 10:02:06 +0000

    python piscine - 0.starting : ex03 : finish

--- python_piscine/0_starting/ex03/NULL_not_found.py
+++ python_piscine/0_starting/ex03/NULL_not_found.py
@@ -2,15 +2,20 @@
 def NULL_not_found(object: any) -> int:
     o_type = type(object)
     types = {
-        None: "Nothing",
         float: "Cheese",
         int: "Zero",
         str: "Empty",
         bool: "Fake"
     }
-    values = (float("NaN"), 0, '', False)
-    if o_type in types and object in values:
+    values = (0, '', False, None)
+    if o_type in types and object in values \
+            or o_type == float and object != object:
         print(types[o_type] + ": ", end = '')
+        print(object, end = ' ')
+        print(o_type)
+        return 0
+    elif object is None:
+        print("Nothing: ", end = '')
         print(object, end = ' ')
         print(o_type)
         return 0
